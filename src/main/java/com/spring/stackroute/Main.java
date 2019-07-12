e84829d4b7be1548f601aa6d8938c26587bfb3cb
NagmaSidrahKhanam <51942983+NagmaSidrahKhanam@users.noreply.github.com>
2019-07-12 10:46:10 +0530

    Update Main.java

--- src/main/java/com/spring/stackroute/Main.java
+++ src/main/java/com/spring/stackroute/Main.java
@@ -23,7 +23,7 @@ import com.spring.stackroute.domain.Movie;
 
 public class Main {
 
-    public  static  void main(String gopal []) {
+    public  static  void main(String args []) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie=(Movie)applicationContext.getBean("movie2");

23c4e60362d8bf23b01a3e7ed0156004a5433d1b
nagma <nagma.sidra@gmail.com>
2019-07-12 11:04:37 +0530

    Spring Task 5

---
+++ src/main/java/com/spring/stackroute/domain/BeanLifeCycleDemo.java
@@ -0,0 +1,29 @@
+package com.spring.stackroute.domain;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
+
+    public void Init ()
+    {
+        System.out.println("Initialization");
+    }
+
+    public  void clean()
+
+    {
+        System.out.println("custom destroy");
+    }
+    public void destroy ()
+    {
+        System.out.println("Destroy");
+    }
+
+    public void afterPropertiesSet () throws Exception {
+        System.out.println("After properties");
+
+    }
+}

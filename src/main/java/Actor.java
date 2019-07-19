f9a0ea337a91a5ec8f54369dc76f9e8d189b42b6
khsidra <nagma.sidra@gmail.com>
2019-07-19 13:01:37 +0530

    issues Resolves

---
+++ src/main/java/Actor.java
@@ -0,0 +1,30 @@
+
+
+public class Actor {
+    private String actorname;
+    private String actorgender;
+    private  int actorage;
+
+    //setter and getter methods
+    public String getActorName() {
+        return actorname;
+    }
+    public void setActorname(String actornname) {
+        this.actorname = actorname;
+    }
+
+    public String getActorGender() {
+        return actorgender;
+    }
+    public void setActorgender(String actorgender) {
+        this.actorgender = actorgender;
+    }
+
+    public int getActorAge() {
+        return actorage;
+    }
+    public void setActorage(int actorage) {
+        this.actorage = actorage;
+    }
+
+}
